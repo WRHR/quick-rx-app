6105bd5dba46ca5b5328a92bc87d72f6a59e6bda
WRHR <wrhreeves@gmail.com>
2020-12-02 22:07:18 -0700

    Change about component to slide up transition overlay

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,30 @@
-import { Box, Container } from "@chakra-ui/react";
-
+import { Box, Button, useDisclosure, Slide } from "@chakra-ui/react";
 
 function About() {
+  const { isOpen, onToggle } = useDisclosure();
+
   return (
-    <Box>
-      <Container>
-        <h3>About Quick-RX</h3>
-        <p>
-          This application allow quick perscription adcive based on patient
-          characteristics and medical history.
-        </p>
-      </Container>
-    </Box>
+    <>
+      <Button onClick={onToggle}>About</Button>
+      <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
+        <Box
+          p="40px"
+          color="white"
+          mt="4"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+        >
+          <h3>About Quick-RX</h3>
+          <p>
+            This application allow quick perscription adcive based on patient
+            characteristics and medical history.
+          </p>
+        </Box>
+      </Slide>
+    </>
   );
 }
 
 export default About;
+
