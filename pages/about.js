b6620ecde8d62682ced7aa9bbf6f9e0ad764dcdd
WRHR <wrhreeves@gmail.com>
2020-12-02 21:39:02 -0700

    Create About page

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h3>About Quick-RX</h3>
+      <p>This application allow quick perscription adcive based on patient characteristics and medical history.</p>
+    </div>
+  );
+}
+
+export default About;
