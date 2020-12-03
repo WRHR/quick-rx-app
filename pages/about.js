a62e27d22bb517545021a039befb692a76a931e6
WRHR <wrhreeves@gmail.com>
2020-12-02 21:52:12 -0700

    Add Chakra components to About page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,17 @@
+import { Box, Container } from "@chakra-ui/react";
+
+
 function About() {
   return (
-    <div>
-      <h3>About Quick-RX</h3>
-      <p>This application allow quick perscription adcive based on patient characteristics and medical history.</p>
-    </div>
+    <Box>
+      <Container>
+        <h3>About Quick-RX</h3>
+        <p>
+          This application allow quick perscription adcive based on patient
+          characteristics and medical history.
+        </p>
+      </Container>
+    </Box>
   );
 }
 
