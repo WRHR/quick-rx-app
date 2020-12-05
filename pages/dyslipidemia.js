358a5946c76bc9be309074b022530a8303c2ec79
WRHR <wrhreeves@gmail.com>
2020-12-04 23:42:53 -0700

    Edit conditionals and add more diagnostic questions

--- pages/dyslipidemia.js
+++ pages/dyslipidemia.js
@@ -17,6 +17,8 @@ export default function Dyslipidemia() {
   const [lifeExp, setLifeExp] = useState(null);
   const [patientCVD, setPatientCVD] = useState(null);
   const [patientCVDSideBar, setPatientCVDSideBar] = useState(null);
+  const [patientCVDRisk, setPatientCVDRisk] = useState(null);
+  const [patientStatinPref, setPatientStatinPref] = useState(null);
   return (
     <Box>
       <PageHeader diesease="Dyslipidemia" />
@@ -85,7 +87,45 @@ export default function Dyslipidemia() {
               Does the patient have CVD per Sidebar 1, DM, or LDL-C level less
               than 4.9 mmol/L (190mg/dL)
             </FormLabel>
-            <RadioGroup onChange={setPatientCVDSideBar} value={patientCVDSideBar}>
+            <RadioGroup
+              onChange={setPatientCVDSideBar}
+              value={patientCVDSideBar}
+            >
+              <HStack>
+                <Radio value="yes">Yes</Radio>
+                <Radio value="no">No</Radio>
+              </HStack>
+            </RadioGroup>
+          </Box>
+        )}
+        {patientCVDSideBar === "yes" && (
+          <Box>
+            <Text>Recommend moderate dose of statin therapy</Text>
+          </Box>
+        )}
+        {patientCVDSideBar === "no" && (
+          <Box>
+            <FormLabel>Is patient's 10-y CVD risk greater than 12%?</FormLabel>
+            <RadioGroup onChange={setPatientCVDRisk} value={patientCVDRisk}>
+              <HStack>
+                <Radio value="yes">Yes</Radio>
+                <Radio value="no">No</Radio>
+              </HStack>
+            </RadioGroup>
+          </Box>
+        )}
+        {patientCVDRisk === "yes" && (
+          <Box>
+            <Text>Recommend moderate dose of statin therapy</Text>
+          </Box>
+        )}
+        {patientCVDRisk === "no" && (
+          <Box>
+            <FormLabel>
+              Is the patient's 10-y risk 6% to 12% and does the patient prefer
+              statin treatment?
+            </FormLabel>
+            <RadioGroup onChange={setPatientStatinPref} value={patientStatinPref}>
               <HStack>
                 <Radio value="yes">Yes</Radio>
                 <Radio value="no">No</Radio>
