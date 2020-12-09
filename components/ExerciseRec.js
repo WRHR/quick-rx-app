68461a7a525c68198bbafd231674bae7fe8ca91b
WRHR <wrhreeves@gmail.com>
2020-12-08 22:49:24 -0700

    Create ExerciseRec component

---
+++ components/ExerciseRec.js
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function ExerciseRec() {
+  return (
+    <Box>
+      <Text>
+        Recommend regular aerobic exercise and smoking cessation(if applicable)
+      </Text>
+    </Box>
+  );
+}
