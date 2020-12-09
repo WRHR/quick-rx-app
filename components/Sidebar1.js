b63f2a07f43d688cd7e56b4cd3a64caa9b2cba1c
WRHR <wrhreeves@gmail.com>
2020-12-08 21:48:18 -0700

    Create Sidebar1

---
+++ components/Sidebar1.js
@@ -0,0 +1,15 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+export default function Sidebar1(){
+  return(
+    <Box>
+      <Heading>CVD and Equivalents</Heading>
+      <Text>MI or ACS</Text>
+      <Text>CABG/PCI</Text>
+      <Text>Stabel CAD (angina or equivalent)</Text>
+      <Text>Stroke/TIA</Text>
+      <Text>PAD (claudication or AAA)</Text>
+      <Text>Does not include asymptomatic incidental finding atherosclerosis (e.g., CAC)</Text>
+    </Box>
+  )
+}
