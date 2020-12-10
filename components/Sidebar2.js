66ac4f699d746d30bc3f7fc22e63c927f26d71c7
WRHR <wrhreeves@gmail.com>
2020-12-09 21:38:10 -0700

    Replace list elements with correct charkra components

--- components/Sidebar2.js
+++ components/Sidebar2.js
@@ -1,4 +1,4 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, UnorderedList, ListItem } from "@chakra-ui/react";
 
 export default function Sidebar2() {
   <Box>
@@ -6,21 +6,21 @@ export default function Sidebar2() {
     <Text>MI or ACS in past 12 months or </Text>
     <Text>Recurrent ACS, MI or stroke or</Text>
     <Text>Known CVD (see Sidebar 1) and any of the following:</Text>
-    <ul>
-      <li>currently smoking</li>
-      <li>DM</li>
-      <li>CKD</li>
-      <li>PAD</li>
-      <li>CABG/PCI</li>
-    </ul>
+    <UnorderedList>
+      <ListItem>currently smoking</ListItem>
+      <ListItem>DM</ListItem>
+      <ListItem>CKD</ListItem>
+      <ListItem>PAD</ListItem>
+      <ListItem>CABG/PCI</ListItem>
+    </UnorderedList>
     <Text>Stepped intensification</Text>
-    <ul>
-      <li>Maximize statin therapy</li>
-      <li>If not already done, maximize statin therapy and add ezetimbe</li>
-      <li>
+    <UnorderedList>
+      <ListItem>Maximize statin therapy</ListItem>
+      <ListItem>If not already done, maximize statin therapy and add ezetimbe</ListItem>
+      <ListItem>
         Consider PCSK9 inhibitor only after maximizing statin therapy and adding
         ezetimibe
-      </li>
-    </ul>
+      </ListItem>
+    </UnorderedList>
   </Box>;
 }
