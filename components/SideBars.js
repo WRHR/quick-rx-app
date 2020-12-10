9736a81c0b8805e031040e4d8a6ff1b486418156
WRHR <wrhreeves@gmail.com>
2020-12-09 21:57:48 -0700

    Create SideBars component, import into flex on Dyslipidemia page

---
+++ components/SideBars.js
@@ -0,0 +1,27 @@
+import { TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import Sidebar1 from './Sidebar1'
+import Sidebar2 from './Sidebar2'
+import Sidebar3 from './Sidebar3'
+
+export default function SideBars(){
+  return(
+    <Tabs>
+      <TabList>
+        <Tab>Sidebar 1</Tab>
+        <Tab>Sidebar 2</Tab>
+        <Tab>Sidebar 3</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Sidebar1 />
+        </TabPanel>
+        <TabPanel>
+          <Sidebar2 />
+        </TabPanel>
+        <TabPanel>
+          <Sidebar3 />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  )
+}
--- pages/dyslipidemia.js
+++ pages/dyslipidemia.js
@@ -6,12 +6,14 @@ import {
   FormControl,
   FormLabel,
   Heading,
+  Flex,
 } from "@chakra-ui/react";
 import PageHeader from "../components/PageHeader";
 import DiagnosticYN from "../components/DiagnosticYN";
 import SeeSidebar3 from "../components/SeeSidebar3";
-import MediterraneanDiet from '../components/MediterraneanDiet'
+import MediterraneanDiet from "../components/MediterraneanDiet";
 import ExerciseRec from "../components/ExerciseRec";
+import SideBars from '../components/SideBars'
 
 export default function Dyslipidemia() {
   const [validAge, setValidAge] = useState(null);
@@ -28,83 +30,96 @@ export default function Dyslipidemia() {
         VA/DoD Clinical practice guideline for managing Dyslipidemia to reduce
         CVD risk
       </Heading>
-      <FormControl pt="10px" m="0 20px" display="flex" flexDir="column">
-        <FormLabel>Is the patient older than 40?</FormLabel>
-        <DiagnosticYN setState={setValidAge} state={validAge} />
-        {validAge === "yes" && (
-          <Box pt="10px">
-            <FormLabel>
-              Does the patient have EF less than 35%, ESKD, or a life expectancy
-              less than 5 years?
-            </FormLabel>
-            <DiagnosticYN setState={setLifeExp} state={lifeExp} />
-          </Box>
-        )}
-        {validAge === "no" && (
-          <Text>Discuss other treatment options with patient</Text>
-        )}
-        {lifeExp === "yes" && (
-          <Text>
-            Discuss lack of evidence demonstrating benefit and continue ongoing
-            care
-          </Text>
-        )}
-        {lifeExp === "no" && (
-          <Box pt="10px">
-            <FormLabel>
-              Does the patient have higher-risk CVD?
-              <Button>Show CVD and Equivalents</Button>
-            </FormLabel>
-            <DiagnosticYN setState={setPatientCVD} state={patientCVD} />
-          </Box>
-        )}
-        {patientCVD === "yes" && (
-          <Box pt="10px">
-            <Text>Recommend stepped intensification</Text>
-            <Text>Maximize statin therapy or add ezetimibe</Text>
-            <Text>
-              Consider PCSK9 inhibitor only after maximizing statin therapy and
-              adding eetimibe
-            </Text>
-            <Text>If MI,ACS, or CABG/PCI in the past 6 weeks, refer for cardiac rehab <Button>?</Button></Text>
-            <MediterraneanDiet />
-          </Box>
-        )}
-        {patientCVD === "no" && (
-          <Box pt="10px">
-            <FormLabel>
-              Does the patient have CVD per Sidebar 1, DM, or LDL-C level less
-              than 4.9 mmol/L (190mg/dL)
-            </FormLabel>
-            <DiagnosticYN
-              setState={setPatientCVDSideBar}
-              state={patientCVDSideBar}
-            />
-          </Box>
-        )}
-        {patientCVDSideBar === "yes" && <SeeSidebar3 />}
-        {patientCVDSideBar === "no" && (
-          <Box pt="10px">
-            <FormLabel>Is patient's 10-y CVD risk greater than 12%?</FormLabel>
-            <DiagnosticYN setState={setPatientCVDRisk} state={patientCVDRisk} />
-          </Box>
-        )}
-        {patientCVDRisk === "yes" && <SeeSidebar3 />}
-        {patientCVDRisk === "no" && (
-          <Box pt="10px">
-            <FormLabel>
-              Is the patient's 10-y risk 6% to 12% and does the patient prefer
-              statin treatment?
-            </FormLabel>
-            <DiagnosticYN
-              setState={setPatientStatinPref}
-              state={patientStatinPref}
-            />
-          </Box>
-        )}
-        {patientStatinPref === "yes" && <SeeSidebar3 />}
-        {patientStatinPref ==='no' && <ExerciseRec />}
-      </FormControl>
+      <Flex>
+        <Box>
+          <FormControl pt="10px" m="0 20px" display="flex" flexDir="column">
+            <FormLabel>Is the patient older than 40?</FormLabel>
+            <DiagnosticYN setState={setValidAge} state={validAge} />
+            {validAge === "yes" && (
+              <Box pt="10px">
+                <FormLabel>
+                  Does the patient have EF less than 35%, ESKD, or a life
+                  expectancy less than 5 years?
+                </FormLabel>
+                <DiagnosticYN setState={setLifeExp} state={lifeExp} />
+              </Box>
+            )}
+            {validAge === "no" && (
+              <Text>Discuss other treatment options with patient</Text>
+            )}
+            {lifeExp === "yes" && (
+              <Text>
+                Discuss lack of evidence demonstrating benefit and continue
+                ongoing care
+              </Text>
+            )}
+            {lifeExp === "no" && (
+              <Box pt="10px">
+                <FormLabel>
+                  Does the patient have higher-risk CVD?
+                  <Button>Show CVD and Equivalents</Button>
+                </FormLabel>
+                <DiagnosticYN setState={setPatientCVD} state={patientCVD} />
+              </Box>
+            )}
+            {patientCVD === "yes" && (
+              <Box pt="10px">
+                <Text>Recommend stepped intensification</Text>
+                <Text>Maximize statin therapy or add ezetimibe</Text>
+                <Text>
+                  Consider PCSK9 inhibitor only after maximizing statin therapy
+                  and adding eetimibe
+                </Text>
+                <Text>
+                  If MI,ACS, or CABG/PCI in the past 6 weeks, refer for cardiac
+                  rehab <Button>?</Button>
+                </Text>
+                <MediterraneanDiet />
+              </Box>
+            )}
+            {patientCVD === "no" && (
+              <Box pt="10px">
+                <FormLabel>
+                  Does the patient have CVD per Sidebar 1, DM, or LDL-C level
+                  less than 4.9 mmol/L (190mg/dL)
+                </FormLabel>
+                <DiagnosticYN
+                  setState={setPatientCVDSideBar}
+                  state={patientCVDSideBar}
+                />
+              </Box>
+            )}
+            {patientCVDSideBar === "yes" && <SeeSidebar3 />}
+            {patientCVDSideBar === "no" && (
+              <Box pt="10px">
+                <FormLabel>
+                  Is patient's 10-y CVD risk greater than 12%?
+                </FormLabel>
+                <DiagnosticYN
+                  setState={setPatientCVDRisk}
+                  state={patientCVDRisk}
+                />
+              </Box>
+            )}
+            {patientCVDRisk === "yes" && <SeeSidebar3 />}
+            {patientCVDRisk === "no" && (
+              <Box pt="10px">
+                <FormLabel>
+                  Is the patient's 10-y risk 6% to 12% and does the patient
+                  prefer statin treatment?
+                </FormLabel>
+                <DiagnosticYN
+                  setState={setPatientStatinPref}
+                  state={patientStatinPref}
+                />
+              </Box>
+            )}
+            {patientStatinPref === "yes" && <SeeSidebar3 />}
+            {patientStatinPref === "no" && <ExerciseRec />}
+          </FormControl>
+        </Box>
+        <SideBars />
+      </Flex>
     </Box>
   );
 }
