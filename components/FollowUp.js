71ce70348d467f31246eff1d0c8d0e9914914377
WRHR <wrhreeves@gmail.com>
2020-12-13 20:59:58 -0700

    Import ui components and replace

--- components/FollowUp.js
+++ components/FollowUp.js
@@ -1,17 +1,17 @@
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Box, Heading, Text, UnorderedList,ListItem } from '@chakra-ui/react'
 
 export default function FollowUp() {
   return(
-    <Box>
-      <Heading>Follow-up Evaluation</Heading>
+    <Box pt='10px'>
+      <Heading size='lg'>Follow-up Evaluation</Heading>
       <Text>Primary Prevention, no statin</Text>
         <Text>Lipid panel every 10 years (nonfasting recommended)</Text>
         <Text>Repeat risk evaluation</Text>
-      <ul>
-        <li>Every 2 years if 6% to 12%</li>        
-        <li>Every 5 years if less than 6%</li>        
-        <li>If risk factors change</li>        
-      </ul>
+      <UnorderedList>
+        <ListItem>Every 2 years if 6% to 12%</ListItem>        
+        <ListItem>Every 5 years if less than 6%</ListItem>        
+        <ListItem>If risk factors change</ListItem>        
+      </UnorderedList>
       <Text>Secondary prevention: lipid panels as needed, only if patient is higher risk and willing to intensify treatment</Text>
       <Text>Once optimal therapy is recieved, no need to recheck lipid levels routinely</Text>
     </Box>
