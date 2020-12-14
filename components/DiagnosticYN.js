ca16fccd6ae03ce44789b3b878316cfc8f672ea2
WRHR <wrhreeves@gmail.com>
2020-12-13 20:43:24 -0700

    Add transitions for diagnostic questions

--- components/DiagnosticYN.js
+++ components/DiagnosticYN.js
@@ -1,14 +1,23 @@
-import { FormLabel, RadioGroup, HStack, Radio, Box } from "@chakra-ui/react";
+import {
+  FormLabel,
+  RadioGroup,
+  HStack,
+  Radio,
+  Box,
+  SlideFade,
+} from "@chakra-ui/react";
 
 export default function DiagnosticYN({ state, setState }) {
   return (
-    <Box>
-      <RadioGroup onChange={setState} value={state}>
-        <HStack>
-          <Radio value="yes">Yes</Radio>
-          <Radio value="no">No</Radio>
-        </HStack>
-      </RadioGroup>
-    </Box>
+    <SlideFade in={true}>
+      <Box>
+        <RadioGroup onChange={setState} value={state}>
+          <HStack>
+            <Radio value="yes">Yes</Radio>
+            <Radio value="no">No</Radio>
+          </HStack>
+        </RadioGroup>
+      </Box>
+    </SlideFade>
   );
 }
