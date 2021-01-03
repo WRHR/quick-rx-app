64babbe893806547c0337d41481ff3dc263fe0a7
WRHR <wrhreeves@gmail.com>
2021-01-02 23:11:34 -0700

    Change SeeSideBar3 component to direct to RevisedFollowUp

--- components/dyslipidemia/SeeSidebar3.js
+++ components/dyslipidemia/SeeSidebar3.js
@@ -1,12 +1,11 @@
 import { Box, Text } from "@chakra-ui/react";
-import Sidebar3 from "./Sidebar3";
-import MediterraneanDiet from './MediterraneanDiet'
+import RevisedFollowUp from "./RevisedFollowUp";
 
 export default function SeeSidebar3() {
   return (
     <Box pt="10px">
       <Text>Recommend moderate dose of statin therapy (see Sidebar 3)</Text>
-      <MediterraneanDiet />
+      <RevisedFollowUp />
     </Box>
   );
 }
